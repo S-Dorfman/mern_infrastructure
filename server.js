9ad764c551c9e70f9cd7451a8cd299edd63d5d37
Shoshana <shoshanadorfman@gmail.com>
2023-04-18 16:32:19 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+//logger middleware
+const logger = require('morgan');
+
+const app = express();
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const PORT = process.env.PORT || 3001;
+
+//* Config 
+// logger middleware 
+app.use(logger('dev'));
+// JSON payload middleware (for data coming from frontend functions)
+app.use(express.json());
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// * ALl API routes
+
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+
+
+app.listen(PORT, function() {
+    console.log(`Express app running on port ${PORT}`)
+  });
